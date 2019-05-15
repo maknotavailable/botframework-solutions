c-jarp/AI

[WIP] Phone: New skill to make phone calls

solutions/Virtual-Assistant/src/csharp/skills/phoneskill/phoneskill/Startup.cs
@@ -1,0 +1,158 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Configuration;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Solutions.Extensions;
+using Microsoft.Bot.Solutions.Middleware;
+using Microsoft.Bot.Solutions.Responses;
+using Microsoft.Bot.Solutions.Skills;
+using Microsoft.Bot.Solutions.Telemetry;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhoneSkill.Dialogs.Main.Resources;
+using PhoneSkill.Dialogs.Sample.Resources;
+using PhoneSkill.Dialogs.Shared.Resources;
+using PhoneSkill.ServiceClients;
+
+namespace PhoneSkill
+{
+    public class Startup
+    {
+        private bool _isProduction = false;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Load the connected services from .bot file.
+            var botFilePath = Configuration.GetSection("botFilePath")?.Value;
+            var botFileSecret = Configuration.GetSection("botFileSecret")?.Value;
+            var botConfig = BotConfiguration.Load(botFilePath ?? @".\PhoneSkill.bot", botFileSecret);
+            services.AddSingleton(sp => botConfig ?? throw new InvalidOperationException($"The .bot config file could not be loaded."));
+
+            // Use Application Insights
+            services.AddBotApplicationInsights(botConfig);
+
+            // Initializes your bot service clients and adds a singleton that your Bot can access through dependency injection.
+            var parameters = Configuration.GetSection("parameters")?.Get<string[]>();
+            var configuration = Configuration.GetSection("configuration")?.GetChildren()?.ToDictionary(x => x.Key, y => y.Value as object);
+            var supportedProviders = Configuration.GetSection("supportedProviders")?.Get<string[]>();
+            var languageModels = Configuration.GetSection("languageModels").Get<Dictionary<string, Dictionary<string, string>>>();
+            var connectedServices = new SkillConfiguration(botConfig, languageModels, supportedProviders, parameters, configuration);
+            services.AddSingleton<SkillConfigurationBase>(sp => connectedServices);
+
+            var responseManager = new ResponseManager(
+                connectedServices.LocaleConfigurations.Keys.ToArray(),
+                new IResponseIdCollection[]
+                {
+                    new MainResponses(),
+                    new SharedResponses(),
+                    new SampleResponses()
+                });
+
+            services.AddSingleton(sp => responseManager);
+
+            var defaultLocale = Configuration.GetSection("defaultLocale").Get<string>();
+
+            // Initialize Bot State
+            var cosmosDbService = botConfig.Services.FirstOrDefault(s => s.Type == ServiceTypes.CosmosDB) ?? throw new Exception("Please configure your CosmosDb service in your .bot file.");
+            var cosmosDb = cosmosDbService as CosmosDbService;
+            var cosmosOptions = new CosmosDbStorageOptions()
+            {
+                CosmosDBEndpoint = new Uri(cosmosDb.Endpoint),
+                AuthKey = cosmosDb.Key,
+                CollectionId = cosmosDb.Collection,
+                DatabaseId = cosmosDb.Database,
+            };
+            var dataStore = new CosmosDbStorage(cosmosOptions);
+            var userState = new UserState(dataStore);
+            var conversationState = new ConversationState(dataStore);
+
+            services.AddSingleton(dataStore);
+            services.AddSingleton(userState);
+            services.AddSingleton(conversationState);
+            services.AddSingleton(new BotStateSet(userState, conversationState));
+
+            services.AddTransient<IServiceManager, ServiceManager>();
+
+            // Add the bot with options
+            services.AddBot<PhoneSkill>(options =>
+            {
+                // Load the connected services from .bot file.
+                var environment = _isProduction ? "production" : "development";
+                var service = botConfig.Services.FirstOrDefault(s => s.Type == ServiceTypes.Endpoint && s.Name == environment);
+                if (!(service is EndpointService endpointService))
+                {
+                    throw new InvalidOperationException($"The .bot file does not contain an endpoint with name '{environment}'.");
+                }
+
+                options.CredentialProvider = new SimpleCredentialProvider(endpointService.AppId, endpointService.AppPassword);
+
+                // Telemetry Middleware (logs activity messages in Application Insights)
+                var sp = services.BuildServiceProvider();
+                var telemetryClient = sp.GetService<IBotTelemetryClient>();
+                var appInsightsLogger = new TelemetryLoggerMiddleware(telemetryClient, logPersonalInformation: true);
+                options.Middleware.Add(appInsightsLogger);
+
+                // Catches any errors that occur during a conversation turn and logs them to AppInsights.
+                options.OnTurnError = async (context, exception) =>
+                {
+                    CultureInfo.CurrentUICulture = new CultureInfo(context.Activity.Locale);
+                    await context.SendActivityAsync(context.Activity.CreateReply(SharedResponses.ErrorMessage));
+                    await context.SendActivityAsync(new Activity(type: ActivityTypes.Trace, text: $"Skill Error: {exception.Message} | {exception.StackTrace}"));
+                    telemetryClient.TrackExceptionEx(exception, context.Activity);
+                };
+
+                // Transcript Middleware (saves conversation history in a standard format)
+                var storageService = botConfig.Services.FirstOrDefault(s => s.Type == ServiceTypes.BlobStorage) ?? throw new Exception("Please configure your Azure Storage service in your .bot file.");
+                var blobStorage = storageService as BlobStorageService;
+                var transcriptStore = new AzureBlobTranscriptStore(blobStorage.ConnectionString, blobStorage.Container);
+                var transcriptMiddleware = new TranscriptLoggerMiddleware(transcriptStore);
+                options.Middleware.Add(transcriptMiddleware);
+
+                // Typing Middleware (automatically shows typing when the bot is responding/working)
+                var typingMiddleware = new ShowTypingMiddleware();
+                options.Middleware.Add(typingMiddleware);
+                options.Middleware.Add(new SetLocaleMiddleware(defaultLocale ?? "en-us"));
+                options.Middleware.Add(new AutoSaveStateMiddleware(userState, conversationState));
+            });
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">Application Builder.</param>
+        /// <param name="env">Hosting Environment.</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            _isProduction = env.IsProduction();
+            app.UseBotApplicationInsights()
+                .UseDefaultFiles()
+                .UseStaticFiles()
+                .UseBotFramework();
+        }
+    }
+}

maknotavailable/botframework-solutions

skills/src/csharp/phoneskill/phoneskill/Startup.cs
@@ -1,0 +1,156 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Configuration;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Builder.Solutions.Extensions;
+using Microsoft.Bot.Builder.Solutions.Middleware;
+using Microsoft.Bot.Builder.Solutions.Responses;
+using Microsoft.Bot.Builder.Solutions.Skills;
+using Microsoft.Bot.Builder.Solutions.Telemetry;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhoneSkill.Dialogs.Main.Resources;
+using PhoneSkill.Dialogs.OutgoingCall.Resources;
+using PhoneSkill.Dialogs.Shared.Resources;
+using PhoneSkill.ServiceClients;
+
+namespace PhoneSkill
+{
+    public class Startup
+    {
+        private bool _isProduction = false;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Load the connected services from .bot file.
+            var botFilePath = Configuration.GetSection("botFilePath")?.Value;
+            var botFileSecret = Configuration.GetSection("botFileSecret")?.Value;
+            var botConfig = BotConfiguration.Load(botFilePath ?? @".\PhoneSkill.bot", botFileSecret);
+            services.AddSingleton(sp => botConfig ?? throw new InvalidOperationException($"The .bot config file could not be loaded."));
+
+            // Use Application Insights
+            services.AddBotApplicationInsights(botConfig);
+
+            // Initializes your bot service clients and adds a singleton that your Bot can access through dependency injection.
+            var parameters = Configuration.GetSection("parameters")?.Get<string[]>();
+            var configuration = Configuration.GetSection("configuration")?.GetChildren()?.ToDictionary(x => x.Key, y => y.Value as object);
+            var supportedProviders = Configuration.GetSection("supportedProviders")?.Get<string[]>();
+            var languageModels = Configuration.GetSection("languageModels").Get<Dictionary<string, Dictionary<string, string>>>();
+            var connectedServices = new SkillConfiguration(botConfig, languageModels, supportedProviders, parameters, configuration);
+            services.AddSingleton<SkillConfigurationBase>(sp => connectedServices);
+
+            var supportedLanguages = connectedServices.LocaleConfigurations.Keys.ToArray();
+            var responseManager = new ResponseManager(
+                supportedLanguages,
+                new MainResponses(),
+                new SharedResponses(),
+                new OutgoingCallResponses());
+
+            services.AddSingleton(sp => responseManager);
+
+            var defaultLocale = Configuration.GetSection("defaultLocale").Get<string>();
+
+            // Initialize Bot State
+            var cosmosDbService = botConfig.Services.FirstOrDefault(s => s.Type == ServiceTypes.CosmosDB) ?? throw new Exception("Please configure your CosmosDb service in your .bot file.");
+            var cosmosDb = cosmosDbService as CosmosDbService;
+            var cosmosOptions = new CosmosDbStorageOptions()
+            {
+                CosmosDBEndpoint = new Uri(cosmosDb.Endpoint),
+                AuthKey = cosmosDb.Key,
+                CollectionId = cosmosDb.Collection,
+                DatabaseId = cosmosDb.Database,
+            };
+            var dataStore = new CosmosDbStorage(cosmosOptions);
+            var userState = new UserState(dataStore);
+            var conversationState = new ConversationState(dataStore);
+
+            services.AddSingleton(dataStore);
+            services.AddSingleton(userState);
+            services.AddSingleton(conversationState);
+            services.AddSingleton(new BotStateSet(userState, conversationState));
+
+            services.AddTransient<IServiceManager, ServiceManager>();
+
+            // Add the bot with options
+            services.AddBot<PhoneSkill>(options =>
+            {
+                // Load the connected services from .bot file.
+                var environment = _isProduction ? "production" : "development";
+                var service = botConfig.Services.FirstOrDefault(s => s.Type == ServiceTypes.Endpoint && s.Name == environment);
+                if (!(service is EndpointService endpointService))
+                {
+                    throw new InvalidOperationException($"The .bot file does not contain an endpoint with name '{environment}'.");
+                }
+
+                options.CredentialProvider = new SimpleCredentialProvider(endpointService.AppId, endpointService.AppPassword);
+
+                // Telemetry Middleware (logs activity messages in Application Insights)
+                var sp = services.BuildServiceProvider();
+                var telemetryClient = sp.GetService<IBotTelemetryClient>();
+                var appInsightsLogger = new TelemetryLoggerMiddleware(telemetryClient, logPersonalInformation: true);
+                options.Middleware.Add(appInsightsLogger);
+
+                // Catches any errors that occur during a conversation turn and logs them to AppInsights.
+                options.OnTurnError = async (context, exception) =>
+                {
+                    CultureInfo.CurrentUICulture = new CultureInfo(context.Activity.Locale);
+                    await context.SendActivityAsync(context.Activity.CreateReply(SharedResponses.ErrorMessage));
+                    await context.SendActivityAsync(new Activity(type: ActivityTypes.Trace, text: $"Skill Error: {exception.Message} | {exception.StackTrace}"));
+                    telemetryClient.TrackExceptionEx(exception, context.Activity);
+                };
+
+                // Transcript Middleware (saves conversation history in a standard format)
+                var storageService = botConfig.Services.FirstOrDefault(s => s.Type == ServiceTypes.BlobStorage) ?? throw new Exception("Please configure your Azure Storage service in your .bot file.");
+                var blobStorage = storageService as BlobStorageService;
+                var transcriptStore = new AzureBlobTranscriptStore(blobStorage.ConnectionString, blobStorage.Container);
+                var transcriptMiddleware = new TranscriptLoggerMiddleware(transcriptStore);
+                options.Middleware.Add(transcriptMiddleware);
+
+                // Typing Middleware (automatically shows typing when the bot is responding/working)
+                var typingMiddleware = new ShowTypingMiddleware();
+                options.Middleware.Add(typingMiddleware);
+                options.Middleware.Add(new SetLocaleMiddleware(defaultLocale ?? "en-us"));
+                options.Middleware.Add(new AutoSaveStateMiddleware(userState, conversationState));
+            });
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">Application Builder.</param>
+        /// <param name="env">Hosting Environment.</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            _isProduction = env.IsProduction();
+            app.UseBotApplicationInsights()
+                .UseDefaultFiles()
+                .UseStaticFiles()
+                .UseBotFramework();
+        }
+    }
+}